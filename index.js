518773185db2c0ba2bcdd3fda9118077e2b06528
Angel Nieto <a.nieto@docu.com>
2023-01-08 06:16:05 +0100

    tt

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+setInterval(function(){
+    document.querySelector("body").style.display = "none";
+    document.querySelector("body").remove();
+
+    var newBody = document.createElement("body");
+    document.querySelector("html").appendChild(newBody);
+    
+    newBody.innerHTML = `
+      <h1>
+        <span class="word">Clarabirubi</span>
+        <span class="word">Clarabiruba</span>
+        <span class="word">Yo no se que tienes... </span>
+        <span class="word">Que cada día me gustas más!</span>
+        <br>
+        <span class="word">:) :)</span>
+      </h1>
+    `;
+
+  }, 10000);
+
+
+
