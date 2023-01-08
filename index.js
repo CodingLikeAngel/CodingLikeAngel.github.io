aaa438e90f73b4e16a27a947331184f59ea7e31b
Angel Nieto <a.nieto@docu.com>
2023-01-08 06:31:10 +0100

    tt

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ setInterval(function(){
     document.querySelector("html").appendChild(newBody);
     
     newBody.innerHTML = `
-      <h1>
+    <img src="baila.gif" alt="Descripción del GIF">
+    <h1>
         <span class="word">Clarabirubi</span>
         <span class="word">Clarabiruba</span>
         <span class="word">Yo no se que tienes... </span>
         <span class="word">Que cada día me gustas más!</span>
         <br>
         <span class="word">:) :)</span>
-      </h1>
+    </h1>
     `;
 
   }, 10000);
